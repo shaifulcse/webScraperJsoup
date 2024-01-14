3fcb8016d9763e151012bbf8843870fc2970feec
Shaiful Chowdhury <msacbd@gmail.com>
2024-01-13 18:25:04 -0600

    some cleaning

--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class Util {
 
     public static void printResults(List<Chair> chairs, List<ResearchLab> researchLabs){
-        System.out.println("\n############## CS, University of Alberta #####################\n");
+        System.out.println("\n                                    ############## CS, University of Alberta #####################\n");
 
-        System.out.println("###########################");
-        System.out.println("Information about the Chairs");
+        System.out.println("                                    ################################################################");
+        System.out.printf("%80s\n\n", "INFORMATION ABOUT THE CHAIRS");
         for (Chair chair: chairs){
-            System.out.println(chair.role+ "\t" + chair.name+"\t"+chair.email);
+            System.out.printf("%-50s\t%-30s\t%-20s\n", chair.role, chair.name, chair.email);
         }
-        System.out.println("###########################\n\n");
+        System.out.println("                                    ################################################################\n\n");
 
-        System.out.println("###########################");
-        System.out.println("Information about the Research Labs");
+        System.out.println("                                    ################################################################");
+        System.out.printf("%80s\n\n", "INFORMATION ABOUT THE RESEARCH LABS");
         for(ResearchLab researchLab: researchLabs){
-            System.out.println(researchLab.labName+ "\t" + researchLab.labLink);
+            System.out.printf("%-40s\t%s\n", researchLab.labName, researchLab.labLink);
         }
-        System.out.println("###########################");
+        System.out.println("\n                                    ################################################################");
 
     }
 }
